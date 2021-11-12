c186733641b95c826d766fd27bc79d1c06b11d15
mohamedreda3 <73368901+mohamedreda3@users.noreply.github.com>
2021-11-12 17:01:42 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hdr from '../cmpnts/hdr/hdr.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hdr />
+    </div>
+  );
+}
+
+export default App;
