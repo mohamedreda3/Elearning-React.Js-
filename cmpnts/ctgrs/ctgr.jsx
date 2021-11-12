1368da84711ecbc57290b8576e9caa4b2de15311
mohamedreda3 <73368901+mohamedreda3@users.noreply.github.com>
2021-11-12 17:06:39 +0200

    Add files via upload

---
+++ cmpnts/ctgrs/ctgr.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ctgrs.css';
+
+export default function(){
+    return(
+                <div className="oslide">
+            <div className="image">
+                <img src="../../cmpnts/images/mx.png" />
+            </div>
+            <div className="slideinfo">
+                <h1 className="oname">Web-Development</h1>
+                <p>The frontend of a software program or website is everything with which the user interacts.
+                    From a user standpoint, the frontend is synonymous with the user interface.</p>
+<strong><p>StudentsNumber : 1500 Student</p></strong>
+<strong><p>(5.0)</p></strong>
+                <a href="#" className="buprod">
+                    <span>V</span>
+                    <span>I</span>
+                    <span>E</span>
+                    <span>W</span>
+                    <span>C</span>
+                    <span>O</span>
+                    <span>U</span>
+                    <span>R</span>
+                    <span>S</span>
+                    <span>E</span>
+                </a>
+            </div>
+        </div>
+    );
+}
---
+++ cmpnts/ctgrs/ctgrs.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Ctgr from './ctgr';
+import './ctgrs.css';
+export default function(){
+    return(
+        <div className="brnctgr">
+        <h2>Our-Courses</h2>
+        <div className="ctgr-col">
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+<Ctgr />
+</div>
+                <a href="#" className="buprod showmore">
+                    <span>S</span>
+                    <span>H</span>
+                    <span>O</span>
+                    <span>W</span>
+                    <span>M</span>
+                    <span>O</span>
+                    <span>R</span>
+                    <span>E</span>
+                </a>
+</div>
+    );
+}
